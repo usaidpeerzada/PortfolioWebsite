63118b507c9828b61ba649c6fe94abefda19e86d
Usaid <usaidpeerzada@gmail.com>
2020-07-14 14:43:40 +0530

    changed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,9 +10,7 @@
 
 <style scoped>
 .footer {
-  position: relative;
-  min-height: 100%;
-  /* padding: 1.5rem 0 0.5rem;
+  padding: 1.5rem 0 0.5rem;
   text-align: center;
   color: white;
   letter-spacing: 0.2rem;
@@ -20,14 +18,8 @@
   position: fixed;
   left: 0;
   bottom: 0;
-  width: 100%; */
+  width: 100%;
   background: #008a96;
   color: white;
-}
-.foot {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  height: 2.5rem;
 }
 </style>
