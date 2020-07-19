708252d4372fe78bcc35d5ea4d0f50f289bbbfae
Usaid <usaidpeerzada@gmail.com>
2020-07-20 00:59:24 +0530

    footer

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto sm:max-h-screen">
+  <div class="container mx-auto sm:max-h-full">
     <div>
       <img src="../assets/res.png" alt="my resume" />
     </div>
