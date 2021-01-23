c8d9dff96755fdcb78cad95c31a34ff3f350cfad
usaidpeerzada <usaidpeerzada@gmail.com>
2021-01-23 19:24:07 +0530

    updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <div class="relative bg-white overflow-hidden">
       <div class="max-w-screen-xl  mx-auto">
         <div
-          class="relative lg:h-screen sm:h-screen z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
+          class="relative lg:h-screen md:h-screen sm:h-screen z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
         >
           <svg
             class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
@@ -251,10 +251,10 @@
 
 <script>
 // @ is an alias to /src
-import Footer from '@/components/Footer.vue';
+import Footer from "@/components/Footer.vue";
 export default {
   components: {
-    Footer
+    Footer,
   },
   data() {
     return { isOpen: false };
