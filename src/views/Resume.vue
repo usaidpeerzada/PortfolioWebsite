180c7cb47a4b1d3c3efac3736de1d43bd103df60
Usaid <46266811+usaidpeerzada@users.noreply.github.com>
2021-10-25 22:07:02 +0530

    updated res

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,20 +1,41 @@
 <template>
   <div class="container mx-auto sm:h-full">
     <NavBar />
-    <div class="h-full w-full">
-      <img src="../assets/resume.png" alt="my resume" />
-    </div>
+    <a
+      id="download-btn"
+      class="
+        text-teal-500
+        bg-teal-300
+        hover:text-teal-600 hover:bg-teal-200
+        py-3
+        px-3
+        focus:outline-none focus:shadow-outline-teal focus:border-teal-300
+        font-medium
+        rounded-md
+      "
+      href="/ResumeUsaidv4.pdf"
+    >
+      Download Resume
+    </a>
+    <pdf class="mt-16" src="/ResumeUsaidv4.pdf"></pdf>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
-
+import pdf from "vue-pdf";
 export default {
   components: {
-    NavBar
-  }
+    NavBar,
+    pdf,
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#download-btn {
+  z-index: 1;
+  position: relative;
+  top: 40px;
+}
+</style>
